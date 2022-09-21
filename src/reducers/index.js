9f751795b4cd5b626af70f58265bbfb0e2f29749
aleksanderjonczyk <o.jonczyk2@gmail.com>
2022-09-21 22:06:55 +0200

    Adjusting posts reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
 
 export default combineReducers({
-  replaceMe: () => "replace pls",
+  posts: postsReducer,
 });
