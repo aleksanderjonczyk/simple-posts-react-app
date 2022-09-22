98976769a1283d09b2720e01aced54d65246f9d9
aleksanderjonczyk <o.jonczyk2@gmail.com>
2022-09-22 21:25:55 +0200

    Adding some more logic

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,5 +3,11 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPosts = () => async (dispatch) => {
   const res = await jsonPlaceholder.get("/posts");
 
-  dispatch({ type: "FETCH_POSTS", payload: res });
+  dispatch({ type: "FETCH_POSTS", payload: res.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const res = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: res.data });
 };
