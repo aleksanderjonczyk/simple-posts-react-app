9da0e7236bcc0293f9854986d2e4ff3a58b5e5f9
aleksanderjonczyk <o.jonczyk2@gmail.com>
2022-09-26 22:29:05 +0200

    Fixing amount of network requests

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
     if (!user) return;
@@ -16,7 +11,7 @@ class UserHeader extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  return { user: state.users.find((user) => user.id === ownProps.userId) };
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
